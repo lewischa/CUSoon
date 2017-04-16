8e63f36e3ec057f7c9625ea8785f52ea728bf518
Jeremy Olsen <jjolsen81@hotmail.com>
2017-04-16 09:57:45 -0700

    NewServiceController
    Added a segue to a new service controller and to a favorites table view
    controller.

---
+++ CUSoon/CUSoon/NewServiceTypeRangeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewServiceTypeRangeViewController.swift
+//  CUSoon
+//
+//  Created by Jeremy Olsen on 4/15/17.
+//  Copyright © 2017 Capstone. All rights reserved.
+//
+
+import UIKit
+
+class NewServiceTypeRangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 43/255, green: 43/255, blue: 43/255, alpha: 1)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
