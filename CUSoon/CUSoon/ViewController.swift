bbb99cb3a9ff9f3c3d7130e3bd8c0683eb7da746
Jeremy Olsen <jjolsen81@hotmail.com>
2017-04-15 18:36:24 -0700

    BackgroundColor
    In viewDidLoad, I set the background to the standard background color.
    Use this as a reference for all other background colors.

--- CUSoon/CUSoon/ViewController.swift
+++ CUSoon/CUSoon/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        view.backgroundColor = UIColor(red: 43/255, green: 43/255, blue: 43/255, alpha: 1)
         // MARK: - Delete All
 //        accessor.dropTable()
         
